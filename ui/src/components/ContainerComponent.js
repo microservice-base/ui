165a2c54b81c579f66d7422c3afff9f4d6284faf
kerami <keramiozsoy@hotmail.com>
2019-04-01 23:53:04 +0300

    created test component for react

---
+++ ui/src/components/ContainerComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ContainerComponent extends Component{
+    render(){
+        return(
+            <h1>container component example</h1>
+        );
+    }
+}
+export default ContainerComponent;
--- ui/src/index.js
+++ ui/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ContainerComponent from './components/ContainerComponent';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ContainerComponent />,document.getElementById('container'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
