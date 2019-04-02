595ca0e9ed7d0d14242e28d22aec7ae40225e9c3
kerami <keramiozsoy@hotmail.com>
2019-04-02 23:59:01 +0300

    react lifecycle

--- ui/src/components/ContainerComponent.js
+++ ui/src/components/ContainerComponent.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
 
-class ContainerComponent extends Component{
+class ContainerComponent extends Component {
+
+    constructor(){
+        super();
+        document.write("constructor neden en alta - render edildikten sonra çalışıyor !!! <br/>");
+    }
+
+    componentWillMount(){
+        // vitual dom işlemleri bittikten sonra  component real-dom a bağlanmadan hemen önce çalışır.
+        document.write("componentWillMount <br/>");
+    }
+    componentWillUnmount(){
+        document.write("componentWillUnmount <br/>");
+    }
+    componentWillUpdate(){
+        document.write("componentWillUpdate <br/>");
+    }
+    componentDidMount(){
+         // vitual dom işlemleri bittikten sonra  component real-dom a bağlanmadan hemen önce çalışır.
+         document.write("componentDidMount <br/>");
+    }
+    componentDidUpdate(){
+        document.write("componentDidUpdate <br/>");
+    }
+    componentDidCatch(){
+        document.write("componentDidCatch <br/>");
+    }
+    
+
+    
+
+
+    //////// shouldComponentUpdate  default true dur  
+    //bir component update edildikten sonra tekrar render edilip dilmeyeceğini belirliyoruz. d 
+    
+    //////// componentWillReceiveProps
+
     render(){
         return(
             <h1>container component example</h1>
+
+            // react lifecycle
+            // mounting
+            // onmouning
+            // update
         );
     }
 }
