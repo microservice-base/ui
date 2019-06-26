718643c4e8dd6cd1d595a52b869c53cebb55228d
kerami <keramiozsoy@hotmail.com>
2019-06-26 22:55:46 +0300

    component - Products

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Product from "./components/Product";
+import Products from './components/Products';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Product />
+        <Products />
       </div>
     );
   }
