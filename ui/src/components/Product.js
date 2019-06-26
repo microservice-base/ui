d13e6a1256ba5805d37cdc8711c92ad21a0a7d46
kerami <keramiozsoy@hotmail.com>
2019-06-26 22:47:18 +0300

    added Product component

---
+++ ui/src/components/Product.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+
+class Product extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <h1>Product</h1>
+        )
+    }
+
+}
+
+export default Product;
